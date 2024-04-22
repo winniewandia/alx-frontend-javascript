0dca97b1bc02706bcf9e8709c7c71d5878b1a853
winniewandia <winniewandia63@gmail.com>
2024-04-22 14:58:30 +0300

    rest parameter

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,10 @@
+export default function returnHowManyArguments (...args) {
+  let number = 0
+  const len = args.length
+
+  for (let i = 0; i < len
+    ; i++) {
+    number += 1
+  }
+  return number
+}
---
+++ 4-main.js
@@ -0,0 +1,4 @@
+import returnHowManyArguments from './0x00-ES6_basic/4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
