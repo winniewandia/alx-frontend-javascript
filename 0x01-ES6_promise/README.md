c5b497fda6c9ae54c8bb3234ce88d2ea2a0cef6e
winniewandia <winniewandia63@gmail.com>
2024-04-23 15:50:24 +0300

    passing an object to resolve and a message to reject

--- 0x01-ES6_promise/README.md
+++ 0x01-ES6_promise/README.md
@@ -1,1 +1,2 @@
 0. returning a promise
+1. passing an object to resolve and a message to reject
---
+++ 1-main.js
@@ -0,0 +1,4 @@
+import getFullResponseFromAPI from './0x01-ES6_promise/1-promise';
+
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
