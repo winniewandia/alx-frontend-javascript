debf4555b2ba3094658a92e83cfbc0481c96b231
winniewandia <winniewandia63@gmail.com>
2024-04-24 14:12:40 +0300

    promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,12 @@
 import { uploadPhoto, createUser } from '../utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((response1) => {
-    createUser().then((response2) => {
-      console.log(`${response1.body} ${response2.firstName} ${response2.lastName}`)
-    });
-  })
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const response1 = values[0];
+      const response2 = values[1];
+      console.log(`${response1.body} ${response2.firstName} ${response2.lastName}`);
+    })
     .catch(() => {
       console.log('Signup system offline');
     });
