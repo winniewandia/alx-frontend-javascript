5c0b84cd0502a354b3d120aaf4744acbe03e30df
winniewandia <winniewandia63@gmail.com>
2024-04-24 13:57:49 +0300

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then((response, reject) => {
+  promise.then((response, reject) => {
     console.log('Got a response from the API');
-    response({ status: 200, body: 'success' });
-    reject(new Error());
-  });
+    return response({ status: 200, body: 'success' });
+  })
+    .catch(() => {
+      return new Error();
+    });
 }
