0d6d9b19b181d647b6473d1dcb81a01c5bda4839
winniewandia <winniewandia63@gmail.com>
2024-04-24 14:09:03 +0300

    using finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((response, reject) => {
-    console.log('Got a response from the API');
-    return response({ status: 200, body: 'success' });
-  })
-    .catch(() => {
-      return new Error();
-    });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
