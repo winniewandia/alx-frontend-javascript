6b28ab54b44c10c2e6b5314548bc924e4725461e
winniewandia <winniewandia63@gmail.com>
2024-05-02 16:49:18 +0300

    new class with undefined attributes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,4 +1,4 @@
-import Car from './10-car.js';
+import Car from "./10-car";
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
@@ -7,6 +7,6 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    return new Car(undefined, undefined, undefined);
+    return new Car();
   }
 }
