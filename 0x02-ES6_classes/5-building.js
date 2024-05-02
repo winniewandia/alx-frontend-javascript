3d01ec912d415ba223e4b69171488b8512987254
winniewandia <winniewandia63@gmail.com>
2024-05-02 13:43:15 +0300

    newline eslint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
     if (
-      this.constructor !== Building &&
-      this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+      this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
     ) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
