814231599b6fab8ccb5c65d99b292ae7ca4d91ac
winniewandia <winniewandia63@gmail.com>
2024-07-17 22:08:23 +0300

    try and catch

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,17 @@
 import { uploadPhoto, createUser } from '../utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  return { photo, user };
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
