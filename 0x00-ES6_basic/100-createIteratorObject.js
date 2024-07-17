9456bd24dbb2f32d52c0c8d754d31bd3b0084501
winniewandia <winniewandia63@gmail.com>
2024-07-17 21:25:32 +0300

    fixed eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,12 @@
 export default function createIteratorObject(report) {
   return {
-    [Symbol.iterator]: function* () {
+    * [Symbol.iterator]() {
       const departments = Object.keys(report.allEmployees);
       for (const department of departments) {
         for (const employee of report.allEmployees[department]) {
           yield employee;
         }
       }
-    }
-  }
+    },
+  };
 }
