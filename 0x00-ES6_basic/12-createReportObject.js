3e0578ff058bee38f62c0a851f1ab42408e1460c
winniewandia <winniewandia63@gmail.com>
2024-07-17 21:57:31 +0300

    create a report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments (employeesList) {
+    getNumberOfDepartments(employeesList) {
       const departments = Object.keys(employeesList);
       return departments.length;
-    }
-  }
+    },
+  };
 }
