9c34818f7cf97c06f09573c9c4ae7b6438d8edf8
winniewandia <winniewandia63@gmail.com>
2024-07-17 21:47:00 +0300

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -2,8 +2,7 @@ export default function returnHowManyArguments(...args) {
   let number = 0;
   const len = args.length;
 
-  for (let i = 0; i < len;
-    i++) {
+  for (let i = 0; i < len; i += 1) {
     number += 1;
   }
   return number;
