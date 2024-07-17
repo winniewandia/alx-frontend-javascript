14ce8646ff47f2de64fbe467fd3426438f626577
winniewandia <winniewandia63@gmail.com>
2024-07-17 22:17:51 +0300

    executing await in parallel

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,12 +2,9 @@ import { uploadPhoto, createUser } from '../utils';
 
 export default async function asyncUploadUser() {
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    return {
-      photo,
-      user,
-    };
+    // Execute in parallel
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return { photo, user };
   } catch (error) {
     return {
       photo: null,
