586b7b30056a2faead5a738c397a93acc8aa3397
winniewandia <winniewandia63@gmail.com>
2024-07-17 22:32:07 +0300

    await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
-import { uploadPhoto, createUser } from '../utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
+  let res = {};
+
   try {
-    // Execute in parallel
-    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-    return { photo, user };
-  } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
   }
+  return res;
 }
